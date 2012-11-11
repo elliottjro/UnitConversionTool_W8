a7f64d8b908258388b28f8f6c66a6708d91b83e3
Elliott <shwodkro@gmail.com>
2012-11-10 16:46:42 -0800

    Initial commit
     Changes to be committed:
       (use "git rm --cached <file>..." to unstage)
    
    	new file:   .gitattributes
    	new file:   .gitignore
    	new file:   App4.sln
    	new file:   App4/App.xaml
    	new file:   App4/App.xaml.cs
    	new file:   App4/App4.csproj
    	new file:   App4/App4_TemporaryKey.pfx
    	new file:   App4/Assets/Logo.png
    	new file:   App4/Assets/SmallLogo.png
    	new file:   App4/Assets/SplashScreen.png
    	new file:   App4/Assets/StoreLogo.png
    	new file:   App4/Class1.cs
    	new file:   App4/Common/StandardStyles.xaml
    	new file:   App4/MainPage.xaml
    	new file:   App4/MainPage.xaml.cs
    	new file:   App4/Package.appxmanifest

---
+++ App4/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App4
+{
+    public class CBConversionType_data
+    {
+        public List<string> conversionTypeList = new List<string>();
+        public string currentSelectedType = "Area";
+
+        public void initConversionData()
+        {
+            string[] conversionTypeArr = { "Area", "Temperatue", "Speed" };
+            conversionTypeList.Add(conversionTypeArr[0]);
+            conversionTypeList.Add(conversionTypeArr[1]);
+            conversionTypeList.Add(conversionTypeArr[2]);
+        }
+    }
+    public class CBConversionTypeArea_data
+    {
+        public List<string> conversionTypeList = new List<string>();
+
+        public void initConversionData()
+        {
+            string[] conversionTypeArr = { "m", "cm", "km" };
+            conversionTypeList.Add(conversionTypeArr[0]);
+            conversionTypeList.Add(conversionTypeArr[1]);
+            conversionTypeList.Add(conversionTypeArr[2]);
+        }
+    }
+
+
+}
---
+++ App4/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public CBConversionType_data conversionType = new CBConversionType_data();
+        public DependencyProperty currentSelected;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.conversionType.initConversionData();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+
+        private void CBConversionType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            /// currentSelected = CBConversionType.GetValue;
+
+                    
+        }
+
+        private void CBConversionType_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create a new combo box, set it's items source, and add a SelectionChanged handler.
+            CBConversionType.ItemsSource = conversionType.conversionTypeList;
+            CBConversionType.SelectionChanged += CBConversionType_SelectionChanged;
+        }
+
+        private void CBConversionFrom_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
